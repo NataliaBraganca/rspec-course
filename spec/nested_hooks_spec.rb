091b68ec8a4bbd9f13d9056ee66f491660c87ba0
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-08-14 15:21:52 +0200

    nested hooks before example and context

---
+++ spec/nested_hooks_spec.rb
@@ -0,0 +1,34 @@
+RSpec.describe "nested hooks" do
+  before(:context) do
+    puts "OUTER Before context"
+  end
+  before(:example) do
+    puts "OUTER Before example"
+  end
+
+  it "does bacis math" do
+    expect(1 + 1).to eq(2)
+  end
+
+  context "with condition A" do
+    before(:context) do
+      puts "INNER Before context"
+    end
+
+    before(:example) do
+      puts "INNER Before example"
+    end
+
+    it "does some more basic math" do
+      expect(1 + 1).to eq(2)
+    end
+
+    it "does subtraction as well" do
+      expect(5 - 3).to eq(2)
+    end
+  end
+end
+
+=begin
+ all outer, all outer level befores are also going to be valid in inner level
+=end
