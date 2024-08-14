7886c98e7cd7552218faacb69145325b298b163c
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-08-14 18:33:11 +0200

    implicit_subject_spec

---
+++ spec/implicit_subject_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe Hash do
+  # let(:my_hash) {{}}
+
+  it "should start off empty" do
+    puts subject
+    puts subject.class
+    expect(subject.length).to eq(0)
+    subject[:some_key] = "Some value"
+  end
+
+  it "is isolated between examples" do
+    expect(subject.length).to eq(0)
+  end
+
+
+end
--- spec/overwriting_let_spec.rb
+++ spec/overwriting_let_spec.rb
@@ -1,9 +1,11 @@
+
 class ProgrammingLanguage
   attr_reader :name
 
   def initialize(name = 'Ruby')
     @name = name
   end
+
 end
 
 RSpec.describe ProgrammingLanguage do
